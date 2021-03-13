e622cb75e814521433b19f84b820a496bb66b670
Douglas Santiago <dougsrodrigues@outlook.com>
2021-03-12 23:00:11 -0300

    implementando typescript

--- src/components/RepositoryItem.tsx
+++ src/components/RepositoryItem.tsx
@@ -1,4 +1,12 @@
-function RepositoryItem(props) {
+interface RespositoryItemProps {
+  repository: {
+    name: string;
+    description: string;
+    html_url: string;
+  }
+}
+
+function RepositoryItem(props: RespositoryItemProps) {
   return (
     <li>
       <strong>{props.repository?.name}</strong>
--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -6,9 +6,14 @@ import '../styles/repositories.scss'
 
 // https://api.github.com/orgs/rocketseat/repos
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
 
 function RespositoryList() {
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos')
