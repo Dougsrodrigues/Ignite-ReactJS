2c85157cfdb11d6e17cc1ecba1c73f7eb3a2db72
Douglas Santiago <dougsrodrigues@outlook.com>
2021-03-16 23:02:17 -0300

    criando e configurando modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import Modal from 'react-modal'
 import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
-import { useState } from "react";
+
+Modal.setAppElement('#root')
 
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
