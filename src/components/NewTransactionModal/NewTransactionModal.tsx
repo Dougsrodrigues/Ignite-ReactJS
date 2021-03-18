f83736ae9a42becade512504645a063aff77bee7
Douglas Santiago <dougsrodrigues@outlook.com>
2021-03-17 22:25:58 -0300

    estilizando inputs e modal

--- src/components/NewTransactionModal/NewTransactionModal.tsx
+++ src/components/NewTransactionModal/NewTransactionModal.tsx
@@ -1,5 +1,8 @@
 import Modal from 'react-modal'
 
+import closeImg from '../../assets/close.svg'
+import { Container } from './styles'
+
 interface NewTransactionModalProps {
   isOpen: boolean;
   onRequestClose: () => void
@@ -11,8 +14,26 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName='react-modal-overlay'
+      className='react-modal-content'
     >
-      <h2>Cadastrar Transação</h2>
+
+      <button type='button' onClick={onRequestClose} className='react-modal-close'>
+        <img src={closeImg} alt="Fechar modal" />
+      </button>
+
+      <Container>
+
+        <h2>Cadastrar Transação</h2>
+
+        <input type="text" placeholder="Título" />
+
+        <input type="number" placeholder="Valor" />
+
+        <input type="text" placeholder="Categoria" />
+
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   )
 }
